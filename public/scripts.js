a96113afb19eab11d520f8b3fc965b34abb5fe2f
Jarrell <jarrell@getflex.com>
2022-10-25 15:05:46 -0400

    update with stars

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ var scene = new THREE.Scene({background: color4});
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(1000, 900);
 document.body.appendChild(renderer.domElement);
 
 let lIsPressed = false;
@@ -52,6 +52,13 @@ var texture3 = textureLoader.load('assets/images/row.jpg');
 var texture4 = textureLoader.load('assets/images/bigwinlogo.jpeg');
 var texture5 = textureLoader.load('assets/images/crayons.jpg');
 
+// var texture0 = textureLoader.load('assets/images/stars.jpg');
+// var texture1 = textureLoader.load('assets/images/stars.jpg');
+var texture2 = textureLoader.load('assets/images/stars.jpg');
+var texture3 = textureLoader.load('assets/images/stars.jpg');
+// var texture4 = textureLoader.load('assets/images/stars.jpg');
+var texture5 = textureLoader.load('assets/images/stars.jpg');
+
 var materials = [
 	new THREE.MeshBasicMaterial({ map: texture0 }),
 	new THREE.MeshBasicMaterial({ map: texture1 }),
@@ -84,8 +91,8 @@ camera.position.z = 5;
 var animate = function () {
 	requestAnimationFrame(animate);
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+	cube.rotation.x += 0.001;
+	cube.rotation.y += 0.001;
 
 	renderer.render(scene, camera);
 };
@@ -96,8 +103,92 @@ cube.scale.y = -10;
 cube.scale.z = -10;
 cube.rotation.x = 1;
 cube.rotation.y = 1;
+cube.rotation.z = 1;
 
 animate();
+
+function rotateToSong(name) {
+
+	const songs = [
+		{
+			title: 'quiet',
+			rotation: {
+				x: 2,
+				y: 1.6,
+				z: 1
+			}
+		},
+		{
+			title: 'catch22',
+			rotation: {
+				x: 4,
+				y: 4.7,
+				z: 1
+			}
+		},
+		{
+			title: 'row',
+			rotation: {
+				x: 3.59,
+				y: 7.89,
+				z: 1
+			}
+		},
+		{
+			title: 'arizona',
+			rotation: {
+				x: 6.8,
+				y: 1.54,
+				z: 1
+			}
+		},
+		{
+			title: 'sixty9',
+			rotation: {
+				x: 6.34,
+				y: 3.11,
+				z: 1
+			}
+		},
+		{
+			title: 'bigwin',
+			rotation: {
+				x: 9.46,
+				y: 3.4,
+				z: 1
+			}
+		}
+	]
+
+	console.log(name)
+
+	let songToUse = {};
+
+	songs.forEach(song => {
+		if (song.title === name) {
+			songToUse = song;
+		}
+	})
+
+	const {x, y, z} = songToUse.rotation;
+  
+	cube.rotation.x = x;
+	cube.rotation.y = y;
+	cube.rotation.z = z;
+
+	songToUse = {}
+
+	return;
+}
+
+const rotationFormButton = document.querySelector("[data-behavior='rotation-form-button']");
+rotationFormButton.addEventListener('onClick', (e) => {
+	handleRotationSubmit();
+})
+const stats = document.querySelector("[data-behavior='rotation-stats:example']") ;
+stats.addEventListener('click', () => {
+
+})
 
 document.onkeydown = checkKey;
 
@@ -131,16 +222,50 @@ function onDocumentMouseDown(event) {
 	}
 }
 
+function twistCube() {
+	cube.rotation.x += 1;
+	cube.rotation.y += 1;
+	cube.rotation.z += 1;
+	// right arrow
+	return;
+}
+
+function logStat(cubeRotation) {
+	const {x, y, z} = cubeRotation;
+	const stats = document.querySelector("[data-behavior='rotation-stats:example']") ;
+	let div = document.createElement("div");
+	div.style = 'background-color: black; color: white';
+	div.innerHTML = `
+	<p class="stat" onClick=${`twistCube(x,y,z)`}>x:${x} | y:${y} | z:${z}</p>`
+	return stats.append(div)
+	;
+}
+
+function handleRotationSubmit() {
+	const x = document.querySelector('#x-input').value;
+	const y = document.querySelector('#y-input').value;
+	const z = document.querySelector('#z-input').value;
+
+	x.focus = false;
+	
+	cube.rotation.x = x;
+	cube.rotation.y = y;
+	cube.rotation.z = z;
+
+	logStat(cube.rotation)
+	return;
+}
+
 function checkKey(e) {
 
 	e = e || window.event;
 
 	if (e.key == 'ArrowUp') {
-		cube.rotation.x += 0.1;
+		cube.rotation.x -= 0.1;
 		// up arrow
 	}
 	else if (e.key == 'ArrowDown') {
-		cube.rotation.x -= 0.1;
+		cube.rotation.x += 0.1;
 		// down arrow
 	}
 	else if (e.key == 'ArrowLeft') {
@@ -151,20 +276,25 @@ function checkKey(e) {
 		cube.rotation.y += 0.1;
 		// right arrow
 	}
-	else if (e.key == 'x') {
-		cube.scale.x += 1;
-		cube.scale.y += 1;
-		cube.scale.z += 1;
-		// right arrow
-	}
-	else if (e.key == 'z') {
-		cube.scale.x -= 1;
-		cube.scale.y -= 1;
-		cube.scale.z -= 1;
-		// right arrow
-	} else if (e.key == 'g') {
-		console.log(cube.geometry.faces)
-	} else if (e.key == 'l') {
+	// else if (e.key == 'x') {
+	// 	cube.scale.x += 1;
+	// 	cube.scale.y += 1;
+	// 	cube.scale.z += 1;
+	// }
+	// else if (e.key == 'z') {
+	// 	cube.scale.x -= 1;
+	// 	cube.scale.y -= 1;
+	// 	cube.scale.z -= 1;
+	// }
+	else if (e.key == 't') {
+		twistCube()
+	// }
+	// else if (e.key == 'l') {
+	// 	logStat(cube.rotation)
+		// console.log(cube.rotation)
+	// } else if (e.key == 'g') {
+	// 	console.log(cube.geometry.faces)
+	} else if (e.key == 'm') {
 		const mouseMoveButton = document.querySelector('.mouseMouse-button');
 		const lkey = document.querySelector('.lkey');
 
